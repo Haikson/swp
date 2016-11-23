d369a8a61caa7eebc35ff96205c36d3ade500160
Kamo Petrosyan <kamo@haikson.com>
2016-11-23 14:15:21 +0000

    modified:   ask/ask/settings.py 	modified:   ask/ask/settings.pyc 	deleted:    ask/db.sqlite3 	modified:   ask/qa/models.py 	modified:   ask/qa/models.pyc

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-likes')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+    likes = models.ForeignKey(User, related_name='question_like_user')
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-added_at', 'title']
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey('Question')
+    author = models.ForeignKey(User)
+
+    class Meta:
+        verbose_name = "Answer"
+        verbose_name_plural = "Answers"
+
+    def __unicode__(self):
+        pass
+    
+
