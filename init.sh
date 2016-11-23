b83fd0175baebac9c4994b164ad54890a51602ed
Kamo Petrosyan <kamo@haikson.com>
2016-11-23 12:08:18 +0000

    4176

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql restart
+mysql -u root -e "create database Ask_db"
+python /home/box/web/ask/manage.py syncdb
